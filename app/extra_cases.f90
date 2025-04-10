148022af89ae1d06f2cb0d6fd64f2c0ce5e36a74
Nicolás Celedón-Hernández <coriollis.nch@gmail.com>
2025-04-10 10:30:03 -0600

    Add extra_cases

---
+++ app/extra_cases.f90
@@ -0,0 +1,151 @@
+program extra_cases
+  !! For specified [[gila_conditions:m]], [[gila_conditions:p]], [[gila_conditions:p]]
+  !! combinations calculates \( y \) and \( \epsilon_{1 \to 4} \) slow-roll parameters.
+  !! Also limit curves.
+  !!
+  !! Also calculates slow-roll curves for each solution
+  use iso_fortran_env,  &
+    only: sp => real32, &
+          dp => real64, &
+          qp => real128,&
+          stdout => output_unit
+  use gila
+  use lib_io
+  implicit none
+
+  integer :: i, k, j, x_c, e_c
+
+  integer, dimension(3), parameter  :: mt = [ 87, 88, 89 ]
+  !! [[gila_conditions:m]]
+  integer, dimension(3), parameter  :: pt = [ 5, 6, 7 ]
+  !! [[gila_conditions:p]]
+  real(qp), dimension(3), parameter :: lt = [ 1.e-17_qp, 1.e-22_qp, 1.e-27_qp ]
+  !! [[gila_conditions:l]]
+
+  real(qp), parameter :: xi = 0.0_qp
+  !! Initial [[gila_friedmann:x]] value
+  real(qp), parameter :: xf = -100.0_qp
+  !! Final [[gila_friedmann:y]] value
+  real(qp), parameter :: yi = 1.0_qp
+  !! Initial [[gila_friedmann:y]] value
+  integer, parameter :: n = 20000
+  !! Number of integration steps
+
+  real(qp), dimension(n), parameter :: x = [( i * (xf - xi)/n, i = 0, n-1 )]
+  !! \( x \) values array
+  real(qp), dimension(n) :: y
+  !! Solution array
+  real(qp), dimension(n, 5) :: slowroll_data
+  !! \( \epsilon_0 \) array
+
+  type(gila_conditions) :: conditions
+  real(qp), parameter :: matter_density = 0.31_qp
+  real(qp), parameter :: rad_density = 8.4e-5_qp
+  real(qp), parameter :: dark_density = 0.69_qp
+
+  character(len=*), parameter :: data_dir = "data/sims/extra_cases"
+  character(len=32) :: filename
+
+  integer :: x_grid
+  integer :: out_id
+  integer :: mp_id, l_id, lim_id
+  integer :: sr
+! ------------------------------------------------------------------------------------ !
+
+call safe_open("mp.dat", mp_id, file_dir=data_dir)
+
+  call save_integral_conditions(x, yi, mp_id)
+  do i = 1, size(mt)
+  do j = 1, size(pt)
+    write(mp_id, *) mt(i), pt(j)
+  end do
+  end do
+
+close(mp_id)
+
+
+call safe_open("l.dat", l_id, file_dir=data_dir)
+
+  call save_integral_conditions(x, yi, l_id)
+  do k = 1, size(lt)
+    write(l_id, *) lt(k)
+  end do
+
+close(l_id)
+
+
+! --- x
+
+
+call safe_open("x_grid.dat", x_grid, file_dir=data_dir)
+
+  call save_integral_conditions(x, yi, x_grid)
+
+  do x_c = 1, size(x)
+    write(x_grid, *) x(x_c)
+  end do
+
+close(x_grid)
+
+! ---
+
+conditions%cosmos="early"
+conditions%beta=0.0_qp
+conditions%l_tilde=0.0_qp
+conditions%r=0
+conditions%s=0
+conditions%Omega_M = matter_density
+conditions%Omega_R = rad_density
+conditions%Omega_dark = dark_density
+
+
+do k = 1, 3
+
+  conditions%l=lt(k)
+
+  do i = 1, size(mt)
+  do j = 1, size(pt)
+
+  conditions%m=mt(i)
+  conditions%p=pt(i)
+
+  write(filename, '(2(a1, i2.2), a1, es7.1e2)') 'm', conditions%m, &
+                                                'p', conditions%p, &
+                                                'l', conditions%l
+
+  call safe_open(trim(filename)//".dat", out_id, file_dir=data_dir)
+
+    y = gila_solution(x, yi, conditions)
+
+    call save_gila_genconditions(conditions, out_id)
+    call save_gila_genconditions(conditions, stdout)
+    call save_integral_conditions(x, yi, out_id)
+
+    call matrix_to_file(y, out_id)
+
+  close(out_id)
+
+  !v - slowroll
+  write(filename, '(a5, 2(a1, i2.2), a1, es7.1e2)') 'slow-', 'm', conditions%m, &
+                                                'p', conditions%p, &
+                                                'l', conditions%l
+
+  call safe_open(trim(filename)//".dat", sr, file_dir=data_dir)
+
+    slowroll_data(:,1:2) = slowroll0(x, y, conditions%l)
+    do e_c = 3, 5
+      slowroll_data(:,e_c) = slowroll(slowroll_data(:,1), slowroll_data(:,e_c-1))
+    end do
+
+    write(sr, *) "# N   ϵ0   ϵ1   ϵ2   ϵ3"
+    call matrix_to_file(slowroll_data, sr)
+
+  close(sr)
+
+  end do
+  end do
+
+end do
+
+
+end program extra_cases
---
+++ scripts/slow-roll_extra.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+
+"""
+Plots slow-roll data
+"""
+
+# Modules
+from os import makedirs
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('grayscale')
+plt.rcParams['text.usetex'] = True
+plt.rcParams['figure.figsize'] = [6.4, 3]
+
+# Data import
+fig_dir = "plots/slowroll_extra_plots"
+data_dir = "data/sims/extra_cases"
+makedirs(fig_dir, exist_ok=True)
+
+# Planck data
+e1_max = 0.0097
+e2 = 0.032
+e2_min = e2 - 0.008
+e2_max = e2 + 0.009
+e3 = 0.19
+e3_min = e3 - 0.53
+e3_max = e3 + 0.55
+
+
+sr_m87p05l17 = np.loadtxt(data_dir+"/slow-m87p05l1.0E-17.dat", comments='#')
+sr_m87p05l22 = np.loadtxt(data_dir+"/slow-m87p05l1.0E-22.dat", comments='#')
+sr_m87p05l27 = np.loadtxt(data_dir+"/slow-m87p05l1.0E-27.dat", comments='#')
+
+sr_m88p06l17 = np.loadtxt(data_dir+"/slow-m88p06l1.0E-17.dat", comments='#')
+sr_m88p06l22 = np.loadtxt(data_dir+"/slow-m88p06l1.0E-22.dat", comments='#')
+sr_m88p06l27 = np.loadtxt(data_dir+"/slow-m88p06l1.0E-27.dat", comments='#')
+
+sr_m89p07l17 = np.loadtxt(data_dir+"/slow-m89p07l1.0E-17.dat", comments='#')
+sr_m89p07l22 = np.loadtxt(data_dir+"/slow-m89p07l1.0E-22.dat", comments='#')
+sr_m89p07l27 = np.loadtxt(data_dir+"/slow-m89p07l1.0E-27.dat", comments='#')
+
+l17_index = [i for i in range(len(sr_m87p05l17[:, 0]))
+             if -60 <= sr_m87p05l17[i, 0] <= -50]
+l22_index = [i for i in range(len(sr_m87p05l22[:, 0]))
+             if -60 <= sr_m87p05l22[i, 0] <= -50]
+l27_index = [i for i in range(len(sr_m87p05l27[:, 0]))
+             if -60 <= sr_m87p05l27[i, 0] <= -50]
+
+# Plotting
+print("Plotting...")
+
+fig, axs = plt.subplots(1, 3, sharey=True, sharex=True, layout="constrained")
+
+axs[0].set_title(r"$l = 1 \times 10^{-17}$")
+axs[0].plot(sr_m87p05l17[l17_index, 0], sr_m87p05l17[l17_index, 2],
+            label=r"$ m=87, p=5   $", color="k", linestyle="solid")
+axs[0].plot(sr_m88p06l17[l17_index, 0], sr_m88p06l17[l17_index, 2],
+            label=r"$ m=88, p=6   $", color="k", linestyle="dashed")
+axs[0].plot(sr_m89p07l17[l17_index, 0], sr_m89p07l17[l17_index, 2],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[1].set_title(r"$l = 1 \times 10^{-22}$")
+axs[1].plot(sr_m87p05l22[l22_index, 0], sr_m87p05l22[l22_index, 2],
+            label=r"$ m=87, p=5 $", color="k", linestyle="solid")
+axs[1].plot(sr_m88p06l22[l22_index, 0], sr_m88p06l22[l22_index, 2],
+            label=r"$ m=88, p=6 $", color="k", linestyle="dashed")
+axs[1].plot(sr_m89p07l22[l22_index, 0], sr_m89p07l22[l22_index, 2],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[2].set_title(r"$l = 1 \times 10^{-27}$")
+axs[2].plot(sr_m87p05l27[l27_index, 0], sr_m87p05l27[l27_index, 2],
+            label=r"$ m=87, p=5 $", color="k", linestyle="solid")
+axs[2].plot(sr_m88p06l27[l27_index, 0], sr_m88p06l27[l27_index, 2],
+            label=r"$ m=88, p=6 $", color="k", linestyle="dashed")
+axs[2].plot(sr_m89p07l27[l27_index, 0], sr_m89p07l27[l27_index, 2],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[1].set_xlabel(r"$N$")
+axs[0].legend(loc="upper left", fontsize=7)
+axs[0].set_ylabel(r"$ \epsilon_1 $")
+for ax in axs:
+    ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+    ax.set_xlim(-60, -50)
+    ax.set_ylim(bottom=0)
+    ax.fill_between([-60, -50], e1_max, 0, alpha=0.14)
+
+plt.savefig(fig_dir+"/e1.pdf")
+
+plt.close()
+
+fig, axs = plt.subplots(1, 3, sharey=True, sharex=True, layout="constrained")
+
+axs[0].set_title(r"$l = 1 \times 10^{-17}$")
+axs[0].plot(sr_m87p05l17[l17_index, 0], sr_m87p05l17[l17_index, 3],
+            label=r"$ m=87, p=5 $", color="k", linestyle="solid")
+axs[0].plot(sr_m88p06l17[l17_index, 0], sr_m88p06l17[l17_index, 3],
+            label=r"$ m=88, p=6 $", color="k", linestyle="dashed")
+axs[0].plot(sr_m89p07l17[l17_index, 0], sr_m89p07l17[l17_index, 3],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[1].set_title(r"$l = 1 \times 10^{-22}$")
+axs[1].plot(sr_m87p05l22[l22_index, 0], sr_m87p05l22[l22_index, 3],
+            label=r"$ m=87, p=5 $", color="k", linestyle="solid")
+axs[1].plot(sr_m88p06l22[l22_index, 0], sr_m88p06l22[l22_index, 3],
+            label=r"$ m=88, p=6 $", color="k", linestyle="dashed")
+axs[1].plot(sr_m89p07l22[l22_index, 0], sr_m89p07l22[l22_index, 3],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[2].set_title(r"$l = 1 \times 10^{-27}$")
+axs[2].plot(sr_m87p05l27[l27_index, 0], sr_m87p05l27[l27_index, 3],
+            label=r"$ m=87, p=5 $", color="k", linestyle="solid")
+axs[2].plot(sr_m88p06l27[l27_index, 0], sr_m88p06l27[l27_index, 3],
+            label=r"$ m=88, p=6 $", color="k", linestyle="dashed")
+axs[2].plot(sr_m89p07l27[l27_index, 0], sr_m89p07l27[l27_index, 3],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[1].set_xlabel(r"$N$")
+axs[0].legend(loc="upper left", fontsize=7)
+axs[0].set_ylabel(r"$ \epsilon_2 $")
+for ax in axs:
+    ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+    ax.hlines(e2, xmin=-60, xmax=-50, alpha=0.4)
+    ax.set_xlim(-60, -50)
+    ax.fill_between([-60, -50], e2_min, e2_max, alpha=0.1)
+
+plt.savefig(fig_dir+"/e2.pdf")
+
+# for ax in axs:
+#     ax.set_ylim(1.6e-2, 2.25e-2)
+#
+# plt.savefig(fig_dir+"/e2_zoom.pdf")
+
+plt.close()
+
+fig, axs = plt.subplots(1, 3, sharey=True, sharex=True, layout="constrained")
+
+axs[0].set_title(r"$l = 1 \times 10^{-17}$")
+axs[0].plot(sr_m87p05l17[l17_index, 0], sr_m87p05l17[l17_index, 4],
+            label=r"$ m=87, p=5 $", color="k", linestyle="solid")
+axs[0].plot(sr_m88p06l17[l17_index, 0], sr_m88p06l17[l17_index, 4],
+            label=r"$ m=88, p=6 $", color="k", linestyle="dashed")
+axs[0].plot(sr_m89p07l17[l17_index, 0], sr_m89p07l17[l17_index, 4],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[1].set_title(r"$l = 1 \times 10^{-22}$")
+axs[1].plot(sr_m87p05l22[l22_index, 0], sr_m87p05l22[l22_index, 4],
+            label=r"$ m=87, p=5 $", color="k", linestyle="solid")
+axs[1].plot(sr_m88p06l22[l22_index, 0], sr_m88p06l22[l22_index, 4],
+            label=r"$ m=88, p=6 $", color="k", linestyle="dashed")
+axs[1].plot(sr_m89p07l22[l22_index, 0], sr_m89p07l22[l22_index, 4],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[2].set_title(r"$l = 1 \times 10^{-27}$")
+axs[2].plot(sr_m87p05l27[l27_index, 0], sr_m87p05l27[l27_index, 4],
+            label=r"$ m=87, p=5 $", color="k", linestyle="solid")
+axs[2].plot(sr_m88p06l27[l27_index, 0], sr_m88p06l27[l27_index, 4],
+            label=r"$ m=88, p=6 $", color="k", linestyle="dashed")
+axs[2].plot(sr_m89p07l27[l27_index, 0], sr_m89p07l27[l27_index, 4],
+            label=r"$ m=89, p=7 $", color="k", linestyle="dotted")
+
+axs[1].set_xlabel(r"$N$")
+axs[0].set_ylabel(r"$ \epsilon_3 $")
+axs[0].legend(loc="upper left", fontsize=7)
+for ax in axs:
+    ax.hlines(e3, xmin=-60, xmax=-50, alpha=0.4)
+    ax.set_xlim(-60, -50)
+    ax.fill_between([-60, -50], e3_min, e3_max, alpha=0.14)
+
+plt.savefig(fig_dir+"/e3.pdf")
+
+# for ax in axs:
+#     ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+#     ax.set_ylim(0.015, 0.0225)
+#
+# plt.savefig(fig_dir+"/e3_zoom.pdf")
+
+plt.close()
