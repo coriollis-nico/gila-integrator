23e5fdf63fb533dad0232b536ec34dd1073e0a11
Nicolás Celedón-Hernández <coriollis.nch@gmail.com>
2025-04-12 10:26:29 -0600

    Adaspt to complete data

--- scripts/sorting.py
+++ scripts/sorting.py
@@ -15,13 +15,13 @@ data_dir = "data/tab/more_cases"
 makedirs(fig_dir, exist_ok=True)
 
 # Planck data
-# e1_max = 0.0097
-# e2 = 0.032
-# e2_min = e2 - 0.008
-# e2_max = e2 + 0.009
-# e3 = 0.19
-# e3_min = e3 - 0.53
-# e3_max = e3 + 0.55
+e1_max = 0.0097
+e2 = 0.032
+e2_min = e2 - 0.008
+e2_max = e2 + 0.009
+e3 = 0.19
+e3_min = e3 - 0.53
+e3_max = e3 + 0.55
 
 data = pd.read_csv(data_dir+"/variance.dat", sep=r"\s+")
 
@@ -36,15 +36,16 @@ data_l22_mp_matrix = data_l22_mp_pivot.to_numpy()
 # NOTE: Asume que no hay hoyos en lista de m o de p
 
 print(data_l22_mp_pivot)
+print(data_l22_mp_matrix)
 
 plt.figure(layout="constrained")
 
-plt.matshow(data_l22_mp_matrix, fignum=0, origin="lower",
-            cmap="gray_r",
+plt.matshow(data_l22_mp_matrix, fignum=0,
+            vmax=0.009**2,
             extent=[data_l22_mp["m"].min(),
                     data_l22_mp["m"].max(),
-                    data_l22_mp["p"].min(),
-                    data_l22_mp["p"].max()])
+                    data_l22_mp["p"].max(),
+                    data_l22_mp["p"].min()])
 
 plt.colorbar()
 
