2a6c0b66a70cd79fffc97f522a3f2d47aab04184
Nicolás Celedón Hernández <nicolas.celedonh@ciencias.unam.mx>
2025-04-14 21:53:42 -0600

    WIP make heatmap from variance

--- scripts/heatmap.py
+++ scripts/heatmap.py
@@ -3,14 +3,13 @@
 # Modules
 from os import makedirs
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('grayscale')
 # plt.rcParams['text.usetex'] = True
-plt.rcParams['figure.figsize'] = [6.4, 3]
+# plt.rcParams['figure.figsize'] = [6.4, 14.4]
 
 # Data import
-fig_dir = "plots/sorting"
+fig_dir = "plots/heatmap"
 data_dir = "data/tab/sr_variance"
 makedirs(fig_dir, exist_ok=True)
 
@@ -25,30 +24,123 @@ e3_max = e3 + 0.55
 
 data = pd.read_csv(data_dir+"/variance.dat", sep=r"\s+")
 
+data_l17 = data[(data["l"] >= 1e-18)]
 data_l22 = data[(data["l"] < 1e-18) & (1e-26 < data["l"])]
+data_l27 = data[(1e-26 >= data["l"])]
 
-data_l22_mp = data_l22.loc[:, ("m", "p", "σ²2")]
-data_l22_mp_pivot = data_l22_mp.pivot(index="p",
+
+data_l17_e1_mp = data_l17.loc[:, ("m", "p", "Δϵ1")]
+data_l17_e1_mp_pivot = data_l17_e1_mp.pivot(index="p",
+                                      columns="m",
+                                      values="Δϵ1")
+
+data_l27_e1_mp = data_l27.loc[:, ("m", "p", "Δϵ1")]
+data_l27_e1_mp_pivot = data_l27_e1_mp.pivot(index="p",
+                                      columns="m",
+                                      values="Δϵ1")
+
+data_l22_e1_mp = data_l22.loc[:, ("m", "p", "Δϵ1")]
+data_l22_e1_mp_pivot = data_l22_e1_mp.pivot(index="p",
+                                      columns="m",
+                                      values="Δϵ1")
+
+
+data_l17_e2_mp = data_l17.loc[:, ("m", "p", "σ²2")]
+data_l17_e2_mp_pivot = data_l17_e2_mp.pivot(index="p",
                                       columns="m",
                                       values="σ²2")
-data_l22_mp_matrix = data_l22_mp_pivot.to_numpy()
 
-# NOTE: Asume que no hay hoyos en lista de m o de p
+data_l22_e2_mp = data_l22.loc[:, ("m", "p", "σ²2")]
+data_l22_e2_mp_pivot = data_l22_e2_mp.pivot(index="p",
+                                      columns="m",
+                                      values="σ²2")
 
-print(data_l22_mp_pivot)
-print(data_l22_mp_matrix)
+data_l27_e2_mp = data_l27.loc[:, ("m", "p", "σ²2")]
+data_l27_e2_mp_pivot = data_l27_e2_mp.pivot(index="p",
+                                      columns="m",
+                                      values="σ²2")
 
-plt.figure(layout="constrained")
 
-plt.matshow(data_l22_mp_pivot, fignum=0,
-            vmax=0.009**2,
-            extent=[data_l22_mp["m"].min(),
-                    data_l22_mp["m"].max(),
-                    data_l22_mp["p"].max(),
-                    data_l22_mp["p"].min()])
+data_l17_e3_mp = data_l17.loc[:, ("m", "p", "σ²3")]
+data_l17_e3_mp_pivot = data_l17_e3_mp.pivot(index="p",
+                                      columns="m",
+                                      values="σ²3")
 
-plt.colorbar()
+data_l22_e3_mp = data_l22.loc[:, ("m", "p", "σ²3")]
+data_l22_e3_mp_pivot = data_l22_e3_mp.pivot(index="p",
+                                      columns="m",
+                                      values="σ²3")
 
-plt.show()
+data_l27_e3_mp = data_l27.loc[:, ("m", "p", "σ²3")]
+data_l27_e3_mp_pivot = data_l27_e3_mp.pivot(index="p",
+                                      columns="m",
+                                      values="σ²3")
+
+
+fig, axs = plt.subplots(3, 3, layout="constrained",
+                        sharey="col", sharex="row")
+
+axs[0, 0].matshow(data_l17_e1_mp_pivot, interpolation="none",
+                    vmin=-e1_max, vmax=0,
+                    extent=[data_l17_e1_mp["m"].min(),
+                            data_l17_e1_mp["m"].max(),
+                            data_l17_e1_mp["p"].max(),
+                            data_l17_e1_mp["p"].min()])
+axs[0, 1].matshow(data_l22_e1_mp_pivot, interpolation="none",
+                    vmin=-e1_max, vmax=0,
+                    extent=[data_l22_e1_mp["m"].min(),
+                            data_l22_e1_mp["m"].max(),
+                            data_l22_e1_mp["p"].max(),
+                            data_l22_e1_mp["p"].min()])
+axs[0, 2].matshow(data_l27_e1_mp_pivot, interpolation="none",
+                    vmin=-e1_max, vmax=0,
+                    extent=[data_l27_e1_mp["m"].min(),
+                            data_l27_e1_mp["m"].max(),
+                            data_l27_e1_mp["p"].max(),
+                            data_l27_e1_mp["p"].min()])
+
+axs[1, 0].matshow(data_l17_e2_mp_pivot, interpolation="none",
+                    vmin=0, vmax=0.009**2,
+                    extent=[data_l17_e2_mp["m"].min(),
+                            data_l17_e2_mp["m"].max(),
+                            data_l17_e2_mp["p"].max(),
+                            data_l17_e2_mp["p"].min()])
+axs[1, 1].matshow(data_l22_e2_mp_pivot, interpolation="none",
+                    vmin=0, vmax=0.009**2,
+                    extent=[data_l22_e2_mp["m"].min(),
+                            data_l22_e2_mp["m"].max(),
+                            data_l22_e2_mp["p"].max(),
+                            data_l22_e2_mp["p"].min()])
+axs[1, 2].matshow(data_l27_e2_mp_pivot, interpolation="none",
+                    vmin=0, vmax=0.009**2,
+                    extent=[data_l27_e2_mp["m"].min(),
+                            data_l27_e2_mp["m"].max(),
+                            data_l27_e2_mp["p"].max(),
+                            data_l27_e2_mp["p"].min()])
+
+axs[2, 0].matshow(data_l17_e3_mp_pivot, interpolation="none",
+                    vmin=0, vmax=0.55**2,
+                    extent=[data_l17_e3_mp["m"].min(),
+                            data_l17_e3_mp["m"].max(),
+                            data_l17_e3_mp["p"].max(),
+                            data_l17_e3_mp["p"].min()])
+axs[2, 1].matshow(data_l22_e3_mp_pivot, interpolation="none",
+                    vmin=0, vmax=0.55**2,
+                    extent=[data_l22_e3_mp["m"].min(),
+                            data_l22_e3_mp["m"].max(),
+                            data_l22_e3_mp["p"].max(),
+                            data_l22_e3_mp["p"].min()])
+axs[2, 2].matshow(data_l27_e3_mp_pivot, interpolation="none",
+                    vmin=0, vmax=0.55**2,
+                    extent=[data_l27_e3_mp["m"].min(),
+                            data_l27_e3_mp["m"].max(),
+                            data_l27_e3_mp["p"].max(),
+                            data_l27_e3_mp["p"].min()])
+
+
+# fig.colorbar(pc, label=r"$ \Delta \epsilon_1 $", extend="max", shrink=0.5)
+
+fig.savefig(fig_dir+"/variance.pdf")
+plt.close(fig)
 
 exit()
