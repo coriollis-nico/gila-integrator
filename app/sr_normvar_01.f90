a10dee011f7338ddedd969cc9e6b7d446c4c8471
Nicolás Celedón-Hernández <nicolas.celedonh@ciencias.unam.mx>
2025-04-27 22:02:07 -0600

    Chunk calcs to have everything in one file

--- app/sr_normvar_01.f90
+++ app/sr_normvar_01.f90
@@ -9,11 +9,14 @@ program sr_normvar_01
   use nv
   implicit none
 
-  integer :: i, k, j, e_c, n_c, n_x
+  integer :: i, k, j, e_c, n_c, n_x, this_div
 
-  integer, dimension(40), parameter  :: mt = [(i, i = 3, 42)]
+  integer, parameter :: divs = 12
+  integer, parameter :: msize = 20
+
+  integer, dimension(divs*msize), parameter  :: mt = [(i, i = 3, divs*msize + 2)]
   !! [[gi_conditions:m]]
-  integer, dimension(10), parameter  :: pt = [(j, j = 1, 10)]
+  integer, dimension(20), parameter  :: pt = [(j, j = 1, 20)]
   !! [[gi_conditions:p]]
   real(qp), dimension(3), parameter :: lt = [1.e-17_qp, 1.e-22_qp, 1.e-27_qp]
   !! [[gi_conditions:l]]
@@ -29,9 +32,9 @@ program sr_normvar_01
 
   real(qp), dimension(n), parameter :: x = [( n_x * (xf- xi)/n, n_x = 0, n-1 )]
   !! \( x \) values array
-  real(qp), dimension(n, size(mt), size(pt), size(lt), -1:3) :: sr_data
+  real(qp), dimension(n, msize, size(pt), size(lt), -1:3) :: sr_data
   !! \( \epsilon_i \) array
-  real(qp), dimension(size(mt), size(pt), size(lt), 3) :: sr_nv
+  real(qp), dimension(msize, size(pt), size(lt), 3) :: sr_nv
 
   real(qp), parameter :: matter_density = 0.31_qp
   real(qp), parameter :: rad_density = 8.4e-5_qp
@@ -41,38 +44,38 @@ program sr_normvar_01
   real(qp), dimension(3), parameter :: sr_max = [0.0097_qp, sr_ref(2) + 0.009_qp, sr_ref(3) + 0.55_qp]
   real(qp), parameter :: n_min = -60.0_qp, n_max = -30.0_qp
 
-  character(len=*), parameter :: data_dir = "data/tab/sr_normvar/01"
+  character(len=*), parameter :: data_dir = "data/tab/sr_normvar"
   integer :: vn1_id, vn2_id, vn3_id, mpl_id
 
 ! ------------------------------------------------------------------------------------ !
 
   call safe_open("mpl.dat", mpl_id, file_dir=data_dir)
 
-  do k = 1, size(lt)
-  do j = 1, size(pt)
-  do i = 1, size(mt)
-    write(mpl_id, '(2(i3), (es8.1e2))') mt(i), pt(j), lt(k)
-  end do
-  end do
-  end do
-
-  close(mpl_id)
-
-
   call safe_open("vn1.dat", vn1_id, file_dir=data_dir)
   call safe_open("vn2.dat", vn2_id, file_dir=data_dir)
   call safe_open("vn3.dat", vn3_id, file_dir=data_dir)
 
+  do this_div = 0, divs-1
 
-  print *, "Finding N, ϵ0"
+    print *, "Finding N, ϵ0"
 
-  do concurrent (k = 1:size(lt))
-    do concurrent (i = 1:size(mt))
+    do concurrent (k = 1:size(lt))
       do concurrent (j = 1:size(pt))
+        do concurrent (i = msize*this_div+1 : msize*(this_div+1))
 
-        sr_data(:, i, j, k, -1:0) = gi_sr0(x, &
-                                          gi_solution(x,&
-                                            yi, &
+          sr_data(:, i, j, k, -1:0) = gi_sr0(x, &
+                                            gi_solution(x,&
+                                              yi, &
+                                              gi_conditions(&
+                                                ld = 1.0_qp, &
+                                                Omega_M = matter_density, &
+                                                Omega_R = rad_density, &
+                                                Omega_dark = dark_density, &
+                                                l = lt(k), &
+                                                p = pt(j), &
+                                                m = mt(i) &
+                                              )&
+                                              ), &
                                             gi_conditions(&
                                               ld = 1.0_qp, &
                                               Omega_M = matter_density, &
@@ -81,36 +84,7 @@ program sr_normvar_01
                                               l = lt(k), &
                                               p = pt(j), &
                                               m = mt(i) &
-                                            )&
-                                            ), &
-                                          gi_conditions(&
-                                            ld = 1.0_qp, &
-                                            Omega_M = matter_density, &
-                                            Omega_R = rad_density, &
-                                            Omega_dark = dark_density, &
-                                            l = lt(k), &
-                                            p = pt(j), &
-                                            m = mt(i) &
-                                            ))
-
-        print '(a, i3)', "m = ", mt(i)
-        print '(a, i3)', "p = ", pt(j)
-        print '(a, es8.1e2)', "l = ", lt(k)
-        print '(a)', "-----------"
-
-      end do
-    end do
-  end do
-!
-!
-  do e_c = 1, 3
-    print *, "Finding ϵ_i:", e_c
-    print *, "--------------------"
-    do concurrent (k = 1:size(lt))
-      do concurrent (i = 1:size(mt))
-        do concurrent (j = 1:size(pt))
-
-          sr_data(:, i, j, k, e_c) = gi_sr(x, sr_data(:, i, j, k, e_c - 1))
+                                              ))
 
           print '(a, i3)', "m = ", mt(i)
           print '(a, i3)', "p = ", pt(j)
@@ -120,43 +94,66 @@ program sr_normvar_01
         end do
       end do
     end do
-  end do
 
-  do e_c = 1, 3
-    print *, "Finding variances:", e_c
-    print *, "--------------------"
-    do concurrent (k = 1:size(lt))
-      do concurrent (i = 1:size(mt))
+
+    do e_c = 1, 3
+      print *, "Finding ϵ_i:", e_c
+      print *, "--------------------"
+      do concurrent (k = 1:size(lt))
         do concurrent (j = 1:size(pt))
+          do concurrent (i = msize*this_div+1 : msize*(this_div+1))
 
-          sr_nv(i, j, k, e_c) = nv_nvar(sr_data(:, i, j, k, -1), &
-                                n_min, n_max, &
-                                sr_data(:, i, j, k, e_c), &
-                                sr_ref(e_c), sr_max(e_c))
+            sr_data(:, i, j, k, e_c) = gi_sr(x, sr_data(:, i, j, k, e_c - 1))
 
-          print '(a, i3)', "m = ", mt(i)
-          print '(a, i3)', "p = ", pt(j)
-          print '(a, es8.1e2)', "l = ", lt(k)
-          print '(a)', "-----------"
+            print '(a, i3)', "m = ", mt(i)
+            print '(a, i3)', "p = ", pt(j)
+            print '(a, es8.1e2)', "l = ", lt(k)
+            print '(a)', "-----------"
 
+          end do
         end do
       end do
     end do
-  end do
 
+    do e_c = 1, 3
+      print *, "Finding variances:", e_c
+      print *, "--------------------"
+      do concurrent (k = 1:size(lt))
+        do concurrent (j = 1:size(pt))
+          do concurrent (i = msize*this_div+1 : msize*(this_div+1))
 
-  do k = 1, size(lt)
-  do j = 1, size(pt)
-  do i = 1, size(mt)
-    write(vn1_id, *) sr_nv(i, j, k, 1)
-    write(vn2_id, *) sr_nv(i, j, k, 2)
-    write(vn3_id, *) sr_nv(i, j, k, 3)
-  end do
-  end do
+            sr_nv(i, j, k, e_c) = nv_nvar(sr_data(:, i, j, k, -1), &
+                                  n_min, n_max, &
+                                  sr_data(:, i, j, k, e_c), &
+                                  sr_ref(e_c), sr_max(e_c))
+
+            print '(a, i3)', "m = ", mt(i)
+            print '(a, i3)', "p = ", pt(j)
+            print '(a, es8.1e2)', "l = ", lt(k)
+            print '(a)', "-----------"
+
+          end do
+        end do
+      end do
+    end do
+
+    do k = 1, size(lt)
+    do j = 1, size(pt)
+    do i = msize*this_div + 1, msize*(this_div+1)
+      write(mpl_id, '(2(i3), (es8.1e2))') mt(i), pt(j), lt(k)
+      write(vn1_id, *) sr_nv(i, j, k, 1)
+      write(vn2_id, *) sr_nv(i, j, k, 2)
+      write(vn3_id, *) sr_nv(i, j, k, 3)
+    end do
+    end do
+    end do
+
   end do
 
   close(vn1_id)
   close(vn2_id)
   close(vn3_id)
+
+  close(mpl_id)
 
 end program sr_normvar_01
