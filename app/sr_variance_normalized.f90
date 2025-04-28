b3f6773ec9163a9abbc0301c24fc07c2b25147fe
Nicolás Celedón-Hernández <nicolas.celedonh@ciencias.unam.mx>
2025-04-27 19:07:49 -0600

    Ya ni sé que hice la vdd

--- app/sr_variance_normalized.f90
+++ app/sr_variance_normalized.f90
@@ -1,4 +1,4 @@
-program sr_variance_normalized
+program sr_normvar
   !! Finds the variance \( \sigma^2 \) of the \( \epsilon_i \) values wrt the PLANCK reported
   !! values.
   use iso_fortran_env,  &
@@ -11,27 +11,24 @@ program sr_variance_normalized
 
   integer :: i, k, j, e_c, n_c, n_x
 
-  integer, dimension(100), parameter  :: mt = [(i, i = 3, 102)]
-  !! [[gila_conditions:m]]
+  integer, dimension(110), parameter  :: mt = [(i, i = 3, 112)]
+  !! [[gi_conditions:m]]
   integer, dimension(10), parameter  :: pt = [(j, j = 1, 10)]
-  !! [[gila_conditions:p]]
-!   real(qp), dimension(1), parameter :: lt = [1.e-17_qp, 1.e-22_qp, 1.e-27_qp]
-  real(qp), dimension(1), parameter :: lt = [1.e-17_qp]
-  !! [[gila_conditions:l]]
+  !! [[gi_conditions:p]]
+  real(qp), dimension(3), parameter :: lt = [1.e-17_qp, 1.e-22_qp, 1.e-27_qp]
+  !! [[gi_conditions:l]]
 
   real(qp), parameter :: xi = 0.0_qp
-  !! Initial [[gila_friedmann:x]] value
+  !! Initial [[gi_friedmann:x]] value
   real(qp), parameter :: xf = -100.0_qp
-  !! Final [[gila_friedmann:y]] value
+  !! Final [[gi_friedmann:y]] value
   real(qp), parameter :: yi = 1.0_qp
-  !! Initial [[gila_friedmann:y]] value
+  !! Initial [[gi_friedmann:y]] value
   integer, parameter :: n = 20000
   !! Number of integration steps
 
   real(qp), dimension(n), parameter :: x = [( n_x * (xf- xi)/n, n_x = 0, n-1 )]
   !! \( x \) values array
-  real(qp), dimension(n, size(mt), size(pt), size(lt)) :: y
-  !! Solutions array
   real(qp), dimension(n, size(mt), size(pt), size(lt), -1:3) :: sr_data
   !! \( \epsilon_i \) array
   real(qp), dimension(size(mt), size(pt), size(lt), 3) :: sr_nv
@@ -67,14 +64,16 @@ program sr_variance_normalized
   call safe_open("vn3.dat", vn3_id, file_dir=data_dir)
 
 
-  print *, "Finding solutions"
-  print *, "--------------------"
+  print *, "Finding N, ϵ0"
 
   do concurrent (k = 1:size(lt))
     do concurrent (i = 1:size(mt))
       do concurrent (j = 1:size(pt))
 
-        y(:, i, j, k) = gi_solution(x, yi, gi_conditions(&
+        sr_data(:, i, j, k, -1:0) = gi_sr0(x, &
+                                          gi_solution(x,&
+                                            yi, &
+                                            gi_conditions(&
                                               ld = 1.0_qp, &
                                               Omega_M = matter_density, &
                                               Omega_R = rad_density, &
@@ -82,8 +81,17 @@ program sr_variance_normalized
                                               l = lt(k), &
                                               p = pt(j), &
                                               m = mt(i) &
-                                              )&
-                                      )
+                                            )&
+                                            ), &
+                                          gi_conditions(&
+                                            ld = 1.0_qp, &
+                                            Omega_M = matter_density, &
+                                            Omega_R = rad_density, &
+                                            Omega_dark = dark_density, &
+                                            l = lt(k), &
+                                            p = pt(j), &
+                                            m = mt(i) &
+                                            ))
 
         print '(a, i3)', "m = ", mt(i)
         print '(a, i3)', "p = ", pt(j)
@@ -93,34 +101,8 @@ program sr_variance_normalized
       end do
     end do
   end do
-
-
-  print *, "Finding N, ϵ0"
-
-  do concurrent (k = 1:size(lt))
-    do concurrent (i = 1:size(mt))
-      do concurrent (j = 1:size(pt))
-
-        sr_data(:, i, j, k, -1:0) = gi_sr0(x, y(:, i, j, k), gi_conditions(&
-                                                              ld = 1.0_qp, &
-                                                              Omega_M = matter_density, &
-                                                              Omega_R = rad_density, &
-                                                              Omega_dark = dark_density, &
-                                                              l = lt(k), &
-                                                              p = pt(j), &
-                                                              m = mt(i) &
-                                                              ))
-
-        print '(a, i3)', "m = ", mt(i)
-        print '(a, i3)', "p = ", pt(j)
-        print '(a, es8.1e2)', "l = ", lt(k)
-        print '(a)', "-----------"
-
-      end do
-    end do
-  end do
-
-
+!
+!
   do e_c = 1, 3
     print *, "Finding ϵ_i:", e_c
     print *, "--------------------"
@@ -177,4 +159,4 @@ program sr_variance_normalized
   close(vn2_id)
   close(vn3_id)
 
-end program sr_variance_normalized
+end program sr_normvar
